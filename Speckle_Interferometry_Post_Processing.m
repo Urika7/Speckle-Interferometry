198c6829e1295446225c68604a300cc5b3bba09c
Urika7 <124655400+Urika7@users.noreply.github.com>
2025-01-28 20:34:01 -0700

    Create Speckle_Interferometry_Post_Processing.m
    Original Commit.
    Imported image and displayed it.

---
+++ Speckle_Interferometry_Post_Processing.m
@@ -0,0 +1,14 @@
+close all
+clear all
+clc
+
+%% Importing Image
+ImageOrig = imread("S.B. A sample Trial 2_0°_0°.jpg");
+
+%% Showing Original Unprocessed Image
+figure
+title("Original Unprocessed Image");
+imshow(ImageOrig);
+
+%% Writing New Output Image
+%imwrite(...);
