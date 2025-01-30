55d34f980caa0aaa6b630f52897c8beccf7bcf9f
Urika7 <124655400+Urika7@users.noreply.github.com>
2025-01-29 19:59:06 -0700

    Update Speckle_Interferometry_Post_Processing.m
    Added phases plot of image.
    
    Added heatmap of image.

--- Speckle_Interferometry_Post_Processing.m
+++ Speckle_Interferometry_Post_Processing.m
@@ -5,10 +5,33 @@ clc
 %% Importing Image
 ImageOrig = imread("S.B. A sample Trial 2_0°_0°.jpg");
 
-%% Showing Original Unprocessed Image
-figure
-title("Original Unprocessed Image");
-imshow(ImageOrig);
+%% Initial Plots/Figure Calculation
+% Original Image Phase
+im_orig_fft = fft(ImageOrig); %Take Fourier Transform of image - FT of Real value produces a complex number at each frequency (each Real part = amplitude of number, each imaginary part contains the phase information)
+im_orig_phases = angle(im_orig_fft); %angle() extracts the phase of a complex number - extracts all the phases of each cell in the image array
+
+
+%% Processing
+
+% Divide 
 
 %% Writing New Output Image
 %imwrite(...);
+
+%% Displaying Figures
+figure
+title("Original Image");
+imshow(ImageOrig);
+
+figure
+title("Original Image Heatmap");
+imshow(ImageOrig,colormap);
+
+figure %For heatmap
+Img_orig_squeezed = squeeze(ImageOrig);
+imshow(Img_orig_squeezed(:,:,1), [], Colormap=hot)
+colormap(hot);
+
+figure
+title("Original Image Phases")
+imshow(im_orig_phases);
