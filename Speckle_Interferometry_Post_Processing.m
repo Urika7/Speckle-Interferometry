eb6003fa807f2f80b1fe8c2c28c7fa00df84b07f
Urika7 <124655400+Urika7@users.noreply.github.com>
2025-01-30 20:22:18 -0700

    Update Speckle_Interferometry_Post_Processing.m
    Iterating through processing images with different physical ND filters. 
    
    Formalised code structure.
    
    Displaying best processed image.

--- Speckle_Interferometry_Post_Processing.m
+++ Speckle_Interferometry_Post_Processing.m
@@ -1,37 +1,77 @@
+%% Description
+% MATLAB Script to post-process Speckle-Interferometry Images
+% Image Processing steps:
+    % 1. Divide Sample-Distorted img by referance speckle image
+
+% Written by: Uri Kaufman 28/01/2025
+
+%% Notes
+% OD 1.0 with polarised camera view gives the best results (first image in
+% array)
+
+
+%% Constants
 close all
 clear all
 clc
 
+NUM_IMAGES = 6;
+IMG_PIX_WIDTH = 2048;
+IMG_PIX_HEIGHT = 2048;
+IMG_PIX_DEPTH = 3;
+
 %% Importing Image
-ImageOrig = imread("S.B. A sample Trial 2_0°_0°.jpg");
 
-%% Initial Plots/Figure Calculation
-% Original Image Phase
-im_orig_fft = fft(ImageOrig); %Take Fourier Transform of image - FT of Real value produces a complex number at each frequency (each Real part = amplitude of number, each imaginary part contains the phase information)
-im_orig_phases = angle(im_orig_fft); %angle() extracts the phase of a complex number - extracts all the phases of each cell in the image array
+%Initiate Img arrays
+Img_Ref_Speckle = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
+Img_Sample_Speckle = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
+Img_Divided = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES); %Sample img / ref img (for post processing)
 
+%Read in all images into arrays
+for i = 1:NUM_IMAGES
+    %Use sprintf() and %d to quickly iterate through file names and read them in
+    Img_Ref_Speckle(:,:,:,i) = imread(sprintf("Referance Speckle (30us, 1.%dOD, pol)_90°.tiff",i-1)); %Referance Speckle
+    Img_Sample_Speckle(:,:,:,i) = imread(sprintf("Sample-Distorted Speckle (30us, 1.%dOD, pol)_90°.tiff",i-1)); %Sample-Distorted Speckle
+end
 
 %% Processing
 
-% Divide 
+% Divide sample-distorted img by referance img
+for j = 1:NUM_IMAGES
+    Img_Divided(:,:,:,j) = Img_Ref_Speckle(:,:,:,j) ./ Img_Sample_Speckle(:,:,:,j);
+end
+
+%% Choosing Best Processed Image (found after looking at output images)
+Best_Output_Img = Img_Divided(:,:,:,1);
+
+%% Initial Plots/Figure Calculation
+% Original Image Phase - not needed
+Img_fft = fftshift(fft(Best_Output_Img)); %Take Fourier Transform of image - FT of Real value produces a complex number at each frequency (each Real part = amplitude of number, each imaginary part contains the phase information)
+                                          % fftshift to shift it so that low freqs are in the center      
+Img_phases = angle(Img_fft); %angle() extracts the phase of a complex number - extracts all the phases of each cell in the image array
 
 %% Writing New Output Image
 %imwrite(...);
 
 %% Displaying Figures
-figure
-title("Original Image");
-imshow(ImageOrig);
+% figure
+% title("Original Image");
+% imshow(Img_Sample_Speckle(:,:,:,1));
+% 
+% figure %For heatmap
+% Img_squeezed = squeeze(Best_Output_Img);
+% imshow(Img_squeezed(:,:,3), [], Colormap=hot);
+% colormap(hot);
 
 figure
-title("Original Image Heatmap");
-imshow(ImageOrig,colormap);
+imagesc(Img_phases);
 
-figure %For heatmap
-Img_orig_squeezed = squeeze(ImageOrig);
-imshow(Img_orig_squeezed(:,:,1), [], Colormap=hot)
-colormap(hot);
+%All output images with varying OD for the ND filters
+% for k = 1:NUM_IMAGES
+%     figure
+%     imshow(Img_Divided(:,:,:,k));
+% end
 
 figure
-title("Original Image Phases")
-imshow(im_orig_phases);
+title("Processed Image (BEST)");
+imshow(Best_Output_Img);
