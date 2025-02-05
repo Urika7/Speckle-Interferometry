d1f04de1de87c463d6b592e3fc620e1026e167ad
Urika7 <124655400+Urika7@users.noreply.github.com>
2025-02-05 16:49:09 -0700

    Update Speckle_Interferometry_Post_Processing.m
    Combined inverse fourier transform with interferometry-speckle division post processing.
    
    Used different colormap options to best show the sample in the final image

--- Speckle_Interferometry_Post_Processing.m
+++ Speckle_Interferometry_Post_Processing.m
@@ -2,13 +2,17 @@
 % MATLAB Script to post-process Speckle-Interferometry Images
 % Image Processing steps:
     % 1. Divide Sample-Distorted img by referance speckle image
+    % 2. Take 2D inverse fourier transform of result
+    % 3. Shift inverse fourier transformed image to align sample on center
+    % of image plane
+    
 
 % Written by: Uri Kaufman 28/01/2025
 
 %% Notes
-% OD 1.0 with polarised camera view gives the best results (first image in
-% array)
-
+% Img_Divided = (Img_Ref_One_Plane - Img_Sample_One_Plane) ./
+% (Img_Ref_One_Plane + Img_Sample_One_Plane);  gave some interesting
+% results
 
 %% Constants
 close all
@@ -23,39 +27,50 @@ IMG_PIX_DEPTH = 3;
 %% Importing Image
 
 %Initiate Img arrays
-Img_Ref_Speckle = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
-Img_Sample_Speckle = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
-Img_Divided = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES); %Sample img / ref img (for post processing)
+Img_Ref = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH);
+Img_Sample = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH);
+Img_Divided = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH); %Sample img / ref img (for post processing)
+Img_Inv_Fourier = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT); %Inverse Fourier of an image (for post processing)
 
-%Read in all images into arrays
-for i = 1:NUM_IMAGES
-    %Use sprintf() and %d to quickly iterate through file names and read them in
-    Img_Ref_Speckle(:,:,:,i) = imread(sprintf("Referance Speckle (33us, 1.0OD, pol, %ddegdiff)_90°.tiff",20*(i-1))); %Referance Speckle
-    Img_Sample_Speckle(:,:,:,i) = imread(sprintf("Sample-Distorted Speckle (33us, 1.0OD, pol, %ddegdiff)_90°.tiff",20*(i-1))); %Sample-Distorted Speckle
-end
+%Read in all images
+Img_Sample = imread("X Sample Distorted Img (500us, 2.5OD)_90°.tiff");
+Img_Ref = imread("Ref Img (500us, 2.5OD)_90°.tiff");
+
+%Extract one plane of images
+Img_Sample_One_Plane = Img_Sample(:,:,1);
+Img_Ref_One_Plane = Img_Ref(:,:,1);
+
+
 
 %% Processing
 %Combine images with varying speckle patterns
-Img_Ref_Combined = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
-Img_Sample_Combined = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
-for t = 1:NUM_IMAGES
-    Img_Ref_Combined = Img_Ref_Combined + Img_Ref_Speckle(:,:,:,t);
-    Img_Sample_Combined = Img_Sample_Combined + Img_Sample_Speckle(:,:,:,t);
-end
-Img_Ref_Combined = Img_Ref_Combined / NUM_IMAGES;
-Img_Sample_Combined = Img_Sample_Combined / NUM_IMAGES;
+% Img_Ref_Combined = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
+% Img_Sample_Combined = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
+% for t = 1:NUM_IMAGES
+%     Img_Ref_Combined = Img_Ref_Combined + Img_Ref_Speckle(:,:,:,t);
+%     Img_Sample_Combined = Img_Sample_Combined + Img_Sample_Speckle(:,:,:,t);
+% end
+% Img_Ref_Combined = Img_Ref_Combined / NUM_IMAGES;
+% Img_Sample_Combined = Img_Sample_Combined / NUM_IMAGES;
 
 % Divide sample-distorted img by referance img
-Img_Divided = Img_Ref_Combined ./ Img_Sample_Combined;
+% Img_Divided = Img_Ref_Combined ./ Img_Sample_Combined;
+Img_Divided = Img_Ref_One_Plane ./ Img_Sample_One_Plane;
+
+%Take inv Fourier transform and shift in spacial plane
+Img_Inv_Fourier = ifftshift(ifft2(Img_Divided));
+
+%Take log of magnitude of inv Fourier image (for graphing)
+Img_log = log(abs(Img_Inv_Fourier));
 
 %% Choosing Best Processed Image (found after looking at output images)
-Best_Output_Img = Img_Divided(:,:,:,1);
+%Best_Output_Img = Img_Divided(:,:,:,1);
 
 %% Initial Plots/Figure Calculation
 % Original Image Phase - not needed
-Img_fft = fft2(fftshift(Img_Sample_Speckle(:,:,:,1))); %Take Fourier Transform of image - FT of Real value produces a complex number at each frequency (each Real part = amplitude of number, each imaginary part contains the phase information)
-                                          % fftshift to shift it so that low freqs are in the center      
-Img_phases = angle(Img_fft); %angle() extracts the phase of a complex number - extracts all the phases of each cell in the image array
+% Img_fft = fft2(fftshift(Img_Sample_Speckle(:,:,:,1))); %Take Fourier Transform of image - FT of Real value produces a complex number at each frequency (each Real part = amplitude of number, each imaginary part contains the phase information)
+%                                           % fftshift to shift it so that low freqs are in the center      
+% Img_phases = angle(Img_fft); %angle() extracts the phase of a complex number - extracts all the phases of each cell in the image array
 
 %% Writing New Output Image
 %imwrite(...);
@@ -79,9 +94,27 @@ Img_phases = angle(Img_fft); %angle() extracts the phase of a complex number - e
 %     imshow(Img_Divided(:,:,:,k));
 % end
 
-figure
-imshow(Img_Divided(:,:,:,1));
+% figure
+% imshow(Img_Divided(:,:,:,1));
 
 % figure
 % title("Processed Image (BEST)");
 % imshow(Best_Output_Img);
+
+%----------------------------------------
+subplot(2, 2, 1);
+imshow(Img_Sample);
+title("Original Image (Fourier Transformed)");
+
+subplot(2, 2, 2);
+imshow(Img_log, []);
+title("Inv Fourier Image (Log scaled)")
+
+subplot(2, 2, 3);
+imshow(rgb2gray(Img_Sample), [], Colormap=hot);
+title("Original Image (Fourier Transformed) Heatmap")
+
+subplot(2, 2, 4);
+Img_squeezed = squeeze(Img_log);
+imshow(Img_squeezed, [], Colormap=parula);
+title("Heatmap of Inv Fourier Image (Log Scaled)");
