4348d8bb15d141d4b7a50b9b47e27f3caa14d1d3
Urika7 <124655400+Urika7@users.noreply.github.com>
2025-02-19 15:13:46 -0700

    Fixed colormap displaying issues
    Can see all figures properly

--- Phase_Reconstruction.m
+++ Phase_Reconstruction.m
@@ -26,12 +26,14 @@ IMG_PIX_DEPTH = 3;
 %% Importing Image
 
 %Initiate Img arrays
-Img_Sample_IP_Array = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);
+Img_Sample_IP_Array = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH, NUM_IMAGES);%For 3 phase shifted IP images
+Img_Orig_Sample = zeros(IMG_PIX_WIDTH, IMG_PIX_HEIGHT, IMG_PIX_DEPTH); %Original Image of Sample (no Interference Pattern)
 
 %Read in all images into arrays
 Img_Sample_IP_Array(:,:,:,1) = imread("F Sample (shift = 0) (pol=45°) (No Speckle) (500us, 0.5OD)_45°.tiff"); 
 Img_Sample_IP_Array(:,:,:,2) = imread("F Sample (shift = 2pi_3) (pol=45°) (No Speckle) (500us, 0.5OD)_45°.tiff"); 
-Img_Sample_IP_Array(:,:,:,3) = imread("F Sample (shift = 4pi_3) (pol=45°) (No Speckle) (500us, 0.5OD)_45°.tiff"); 
+Img_Sample_IP_Array(:,:,:,3) = imread("F Sample (shift = 4pi_3) (pol=45°) (No Speckle) (500us, 0.5OD)_45°.tiff");
+Img_Orig_Sample(:,:,:) = imread("F Sample Orig (500us, 0.5OD)_45°.tiff");
 
 %% Calculating Phase
 
@@ -48,24 +50,29 @@ phi = atan(sqrt(3)*(I_4pi_3 - I_0)./(I_4pi_3 + I_0 - 2*I_2pi_3));
 %imwrite(...);
 
 %% Displaying Figures
+colormap("gray");
 
 subplot(2, 3, 1);
-imshow(I_0);
+imagesc(I_0(:,:,1));
 title("F Sample Interference Pattern (0 rad phase shift)");
 
 subplot(2, 3, 2);
-imshow(I_2pi_3);
+imagesc(I_2pi_3(:,:,1));
 title("F Sample Interference Pattern (2pi/3 rad phase shift)")
 
 subplot(2, 3, 3);
-imshow(I_4pi_3);
+imagesc(I_4pi_3(:,:,1));
 title("F Sample Interference Pattern (4pi/3 rad phase shift)")
 
 subplot(2, 3, 4);
+imagesc(Img_Orig_Sample(:,:,1));
+title("Original Image");
+
+subplot(2, 3, 5);
 imshow(phi);
 title("Phase Reconstruction");
 
-subplot(2, 3, 5);
+subplot(2, 3, 6);
 Phi_squeezed = squeeze(phi(:,:,1));%Extracting one layer of the RGB truecolor image for heatmap
 imshow(Phi_squeezed, [], Colormap=hot);
 title("Phase Reconstruction (Heatmap)");
